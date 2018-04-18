c29438c2594c3f22ee0bc9ffe8c0426bb2b16b5f
Brandon Sears <brandonsears8@gmail.com>
2018-04-18 09:00:20 -0400

    add to styling

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,15 +6,19 @@ var navOpenRotate = 0
 nav.style.opacity = '0'
 function showNav() {
   if (nav.style.opacity === '0') {
+    nav.style.visibility = 'visible'
     nav.style.opacity = '1'
     navOpenRotate += 90
     if (navOpenRotate === 360) {
       navOpenRotate = 0
     }
     navOpener.style.transform = `rotate(${navOpenRotate}deg)`
+    navOpener.style.color = '#fff'
   } else {
+    nav.style.visibility = 'hidden'
     nav.style.opacity = '0'
     navOpenRotate += 90
     navOpener.style.transform = `rotate(${navOpenRotate}deg)`
+    navOpener.style.color = '#333'
   }
 }
