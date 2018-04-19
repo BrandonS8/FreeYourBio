76d0ae487bdf00ba92ae37ccfe02441b9b9e1b59
Brandon Sears <brandonsears8@gmail.com>
2018-04-19 16:33:20 -0400

    change precompile

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,10 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.serve_static_assets = true
 module Project4
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
